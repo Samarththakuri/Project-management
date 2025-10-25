cf008f0125e909c8a2e1670d3ee6a44388513f9b
KaachiMitti <samarththakuri98@gmail.com>
2025-10-25 12:49:19 +0530

    learnt how to send email

--- src/utils/mail.js
+++ src/utils/mail.js
@@ -1,5 +1,38 @@
+import { text } from "express";
 import Mailgen from "mailgen";
 
+import nodemailer from "nodemailer";
+const sendEmail = async (options) => {
+  new Mailgen({
+    theme: "default",
+    product: {
+      name: "Task Manager",
+      link: "https://taskmanagelink.com",
+    },
+  });
+  const emailTextual = mailGenetor.generatePlaintext(options.mailgenContent);
+  const emailHtml = mailGenetor.generate(options.mailgenContent);
+  const transporter = nodemailer.createTransport({
+    host: process.env.MAILTRAP_SMTP_HOST,
+    port: process.env.MAILTRAP_SMTP_PORT,
+    auth: {
+      user: process.env.MAILTRAP_SMTP_USER,
+      pass: process.env.MAILTRAP_SMTP_PASS,
+    },
+  });
+  const mail = {
+    from: "mail.taskmanager@example.com",
+    to: options.email,
+    subject: options.subject,
+    text: emailTextual,
+    html: emailHtml,
+  };
+  try {
+    await transporter.sendMail(mail);
+  } catch (error) {
+    console.error("Email service failed");
+  }
+};
 const emailVerificationMailgenContent = (username, verificationUrl) => {
   return {
     body: {
@@ -9,7 +42,7 @@ const emailVerificationMailgenContent = (username, verificationUrl) => {
         instructions:
           "To verify your email please click on the following button",
         button: {
-          color: "#1aae5aff",
+          color: "#1aae5a",
           text: "Verify your email",
           link: verificationUrl,
         },
@@ -27,7 +60,7 @@ const forgotPasswordMailgenContent = (username, passwordResetUrl) => {
       action: {
         instructions: "To reset you password click on the following button",
         button: {
-          color: "#37137aff",
+          color: "#37137a",
           text: "Reset password",
           link: passwordResetUrl,
         },
@@ -36,4 +69,8 @@ const forgotPasswordMailgenContent = (username, passwordResetUrl) => {
     },
   };
 };
-export { emailVerificationMailgenContent, forgotPasswordMailgenContent };
+export {
+  emailVerificationMailgenContent,
+  forgotPasswordMailgenContent,
+  sendEmail,
+};
